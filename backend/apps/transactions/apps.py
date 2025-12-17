246efd4cbdeb306830467d1001377016b9a4e001
xavierAshkar <xavierashkar@gmail.com>
2025-12-16 18:56:20 -0800

    Update apps.py for all apps to correct path | Issue #2|

--- backend/apps/transactions/apps.py
+++ backend/apps/transactions/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class TransactionsConfig(AppConfig):
-    name = "transactions"
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.transactions"
