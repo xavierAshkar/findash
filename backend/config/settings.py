bede81d0086085bfbb06f76148ae2409eba44e6f
xavierAshkar <xavierashkar@gmail.com>
2025-12-16 18:53:38 -0800

    Add models to settings.py | Issue #2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Local apps
+    "apps.accounts",
+    "apps.transactions",
+    "apps.goals",
 ]
 
 MIDDLEWARE = [
