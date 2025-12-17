94e3b5a0b1759641d2cd198b77f528612195b461
xavierAshkar <xavierashkar@gmail.com>
2025-12-16 18:45:58 -0800

    Initialize Transaction domain model | Issue #2

--- backend/apps/transactions/models.py
+++ backend/apps/transactions/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from apps.accounts.models import Account
 
 # Create your models here.
+class Transaction(models.Model):
+    INCOME = "income"
+    EXPENSE = "expense"
+    TRANSFER = "transfer"
+
+    TRANSACTION_TYPE_CHOICES = [
+        (INCOME, "Income"),
+        (EXPENSE, "Expense"),
+        (TRANSFER, "Transfer"),
+    ]
+
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name="transactions",
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    transaction_type = models.CharField(
+        max_length=20,
+        choices=TRANSACTION_TYPE_CHOICES,
+    )
+
+    # Only set if this is part of a transfer
+    related_transaction = models.OneToOneField(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.date} | {self.transaction_type} | {self.amount}"
