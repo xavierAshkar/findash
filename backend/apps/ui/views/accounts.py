81269d4db0b07d6b525cc308674affbb4eae2eb3
xavierAshkar <xavierashkar@gmail.com>
2026-01-14 00:41:48 -0800

    Accounts page v0: Display accounts in basic list format

--- backend/apps/ui/views/accounts.py
+++ backend/apps/ui/views/accounts.py
@@ -1,6 +1,18 @@
+# backend/apps/ui/views/accounts.py
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from apps.accounts.models import Account
+
 @login_required
 def accounts(request):
-    return render(request, "app/accounts/index.html")
+    accounts_qs = (
+        Account.objects
+        .filter(user=request.user)
+        .order_by("account_type", "name")
+    )
+    return render(
+        request,
+        "app/accounts/index.html",
+        {"accounts": accounts_qs},
+    )
